364daa325c01d8fc73dce0e34bb2dbd96eec85b2
Simon Heath <icefoxen@gmail.com>
2015-03-26 08:26:31 -0400

    Guessing game in rust (sort of)

---
+++ rust/guessing/src/main.rs
@@ -0,0 +1,8 @@
+#![feature(old_io)]
+use std::old_io;
+
+fn main() {
+    println!("Type something!");
+    let input = old_io::stdin().read_line().ok().expect("Failed to read line?");
+    println!("You typed: {}", input);
+}
