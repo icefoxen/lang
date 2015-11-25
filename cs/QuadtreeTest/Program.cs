460db5e99b9d9cbe3bf634d9d8d9f603fb99523c
Simon Heath <icefoxen@gmail.com>
2015-11-25 14:48:04 -0500

    Messin' with quadtrees!

---
+++ cs/QuadtreeTest/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace QuadtreeTest
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			RunTest(1000000);
+		}
+
+		static void RunTest(int n, int bounds = 1000000000)
+		{
+			int halfBounds = bounds / 2;
+			var r = new Random();
+			var q = new QuadTree<int>(new Vector2d(0, 0), 0);
+			var sw = new Stopwatch();
+			sw.Start();
+			for (int i = 0; i < n; i++) {
+				q.Insert(new Vector2d(r.NextDouble() * bounds - halfBounds, r.NextDouble() * bounds - halfBounds), 0);
+			}
+			sw.Stop();
+			Console.WriteLine("Adding {0} items to quadtree: {1} seconds", n, sw.ElapsedMilliseconds / 1000.0);
+			
+		}
+	}
+}
