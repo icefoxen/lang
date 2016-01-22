a663ba41d043a8f6aba7be90a1f3732d564276e2
Simon Heath <icefoxen@gmail.com>
2016-01-22 13:53:34 -0500

    Calculator!
    We can at least push strings to a list now!

--- rust/calc/src/main.rs
+++ rust/calc/src/main.rs
@@ -1,3 +1,25 @@
+use std::io;
+
+fn read_something() -> String {
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer)
+    	.unwrap();
+    // trim() returns a &str, but that's just a reference.
+    // So if we just return a &str then the String we allocated
+    // goes poof and it's invalid.  I think.
+    // to_owned() turns a &str into a String.
+    // Possibly by copying.
+    buffer.trim().to_owned()
+}
+
 fn main() {
-    println!("Hello, world!");
+	let mut buffer : Vec<String> = Vec::new();
+    loop {
+	    println!("Type something!");
+		let res = read_something();
+		if res != "" {
+			buffer.push(res);
+		}
+		println!("You typed: {:?}", buffer);
+	}
 }
