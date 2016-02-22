ba9bbc96b4524e0b37e2e519136a57e1fe20d779
Simon Heath <icefoxen@gmail.com>
2016-02-22 22:20:58 +0000

    Messed with dojo a bit more

--- js/dojo/demo/myModule.js
+++ js/dojo/demo/myModule.js
@@ -1,17 +1,27 @@
 define([
     // Dependency list
-    'dojo/dom'
+    'dojo/dom',
+    'dojo/fx',
+    'dojo/domReady!'
 
-], function(dom) {
+], function(dom, fx) {
     // Callback function that is called once this
     // module is done loading.
     var oldText = {};
     // Return an object that is the value of this module
     return {
 	setText: function (id, text) {
-	    var node = dom.byId(id);
-	    oldText[id] = node.innerHTML;
-	    node.innerHTML = text;
+	    var greeting = dom.byId(id);
+	    oldText[id] = greeting.innerHTML;
+	    greeting.innerHTML = text;
+
+	    // Everything's better with animation, right?
+	    // Doesn't seem to work currently, but, forget it.
+	    fx.slideTo({
+		node: greeting,
+		top: 100,
+		left: 200
+	    }).play();
 	},
 
 	restoreText: function (id) {
